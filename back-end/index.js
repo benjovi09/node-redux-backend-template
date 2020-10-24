37ecb55dfbab0fcba8638b8014ff4cb38966f872
Ben <ishouldreadmore@gmail.com>
2020-10-24 12:41:09 -0500

    wip - add remote redux devtools

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import server from './src/server.js';
 import reducer from './src/reducer.js';
 
-const store = createStore(reducer);
+import { composeWithDevTools } from 'remote-redux-devtools';
+
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
+const store = createStore(
+  reducer,
+  /* preloadedState, */ composeEnhancers(
+    applyMiddleware(),
+    // other store enhancers if any
+  ),
+);
+
 server.startServer(store);
